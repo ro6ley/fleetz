e75e9729a7bc62984149e06ee5005116d83559c0
Robley G <robleyadrian@gmail.com>
2020-06-21 18:54:35 +0300

    configure monitoring via sentry

--- fleetz/settings.py
+++ fleetz/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -213,6 +216,17 @@ MAX_ATTEMPTS = 5
 CRON_CLASSES = [
     "fleetz.tasks.MyCronJob",
 ]
+
+# Sentry
+# ------
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN", None),
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # whitenoise
 # ----------
