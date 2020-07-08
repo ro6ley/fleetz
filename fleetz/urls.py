4870987ac3a96f610b5201fbaea58d697ff7711d
Robley G <robleyadrian@gmail.com>
2020-07-08 22:38:15 +0300

    add url for manual tweet scheduling

--- fleetz/urls.py
+++ fleetz/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import ProfileView, HomeView, ProfileFormView, DisconnectView, UnscheduleTweetView
+import fleetz.views as views
 
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
+    path('', views.HomeView.as_view(), name='home'),
     path('accounts/', include('allauth.urls')),
     path('admin/', admin.site.urls),
-    path('profile/', ProfileView.as_view(), name='user_profile'),
-    path('disconnect/<int:user_id>/', DisconnectView.as_view(), name="disconnect"),
-    path('unschedule/<str:tweet_id>/', UnscheduleTweetView.as_view(), name="unschedule"),
+    path('profile/', views.ProfileView.as_view(), name='user_profile'),
+    path('schedule/', views.ScheduleView.as_view(), name='schedule_tweet'),
+    path('disconnect/<int:user_id>/', views.DisconnectView.as_view(), name="disconnect"),
+    path('unschedule/<str:tweet_id>/', views.UnscheduleTweetView.as_view(), name="unschedule"),
 ]
